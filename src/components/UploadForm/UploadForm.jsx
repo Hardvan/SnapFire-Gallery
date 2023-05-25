b350eadb4c97908b3f1ed1b164275b700a1ae3a0
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-25 16:06:13 +0530

    Add Password Field

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import "./UploadForm.css";
+// import "./UploadForm.css";
 import ProgressBar from "../ProgressBar/ProgressBar";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
+  const [password, setPassword] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
 
   const handleChange = (e) => {
     const selected = e.target.files[0];
@@ -12,6 +14,7 @@ const UploadForm = () => {
 
     const types = ["image/png", "image/jpeg", "image/jpg"];
 
+    // File selected & valid type
     if (selected && types.includes(selected.type)) {
       setFile(selected);
     } else {
@@ -20,18 +23,49 @@ const UploadForm = () => {
     }
   };
 
+  const handlePasswordChange = (e) => {
+    const enteredPassword = e.target.value;
+
+    if (enteredPassword === "thisisaverysecretpassword") {
+      setAuthenticated(true);
+      setError(null);
+    } else {
+      setAuthenticated(false);
+      setError("Incorrect password");
+    }
+
+    setPassword(enteredPassword);
+  };
+
   return (
     <form>
       <label>
-        <input type="file" accept="image/*" onChange={handleChange} />
-        <span>+</span>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
       </label>
+      {/* If authenticated, show upload form */}
+      {authenticated && (
+        <label>
+          <input type="file" accept="image/*" onChange={handleChange} />
+          <span>+</span>
+        </label>
+      )}
       <div className="output">
-        {/* If no file selected */}
-        {error && <div className="error">{error}</div>}
-        {/* If file selected */}
-        {file && <div>{file.name}</div>}
-        {file && <ProgressBar file={file} setFile={setFile} />}
+        {authenticated ? (
+          <>
+            {/* If no file selected */}
+            {error && <div className="error">{error}</div>}
+            {/* If file selected */}
+            {file && <div>{file.name}</div>}
+            {file && <ProgressBar file={file} setFile={setFile} />}
+          </>
+        ) : (
+          <div className="error">{error}</div>
+        )}
       </div>
     </form>
   );
